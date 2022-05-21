b0becd985fb5269b03b1dcb75d75a81426376deb
Ri7vaI <Altair1-3@yandex.ru>
2022-05-21 04:11:46 +0300

    Create profile page with window to publication somewhing Create states

--- Perfection-chat/src/app/home/home.module.ts
+++ Perfection-chat/src/app/home/home.module.ts
@@ -6,15 +6,20 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './components/header/header.component';
-
+import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
+import { ModalComponent } from './components/start-post/modal/modal.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [
+    HomePage,
+    HeaderComponent,
+    ProfileSummaryComponent,
+    StartPostComponent,
+    AdvertisingComponent,
+    ModalComponent,
   ],
-  declarations: [HomePage, HeaderComponent]
 })
 export class HomePageModule {}
