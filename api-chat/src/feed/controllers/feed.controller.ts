5acb3bc95bb415957d096a40c40582b0b56f915d
Ri7vaI <Altair1-3@yandex.ru>
2022-05-22 22:33:49 +0300

    Add view post from DB, rebuild scss files to white theme

--- api-chat/src/feed/controllers/feed.controller.ts
+++ api-chat/src/feed/controllers/feed.controller.ts
@@ -1,34 +1,52 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, } from '@nestjs/common';
-  import { Observable } from 'rxjs';
-  import { DeleteResult, UpdateResult } from 'typeorm';
-  
-  import { FeedPost } from '../models/post.interface';
-  import { FeedService } from '../services/feed.service';
-  
-  @Controller('feed')
-  export class FeedController {
-    constructor(private feedService: FeedService) {}
-  
-    @Post()
-    create(@Body() feedPost: FeedPost): Observable<FeedPost> {
-      return this.feedService.createPost(feedPost);
-    }
-  
-    @Get()
-    findAll(): Observable<FeedPost[]> {
-      return this.feedService.findAllPosts();
-    }
-  
-    @Put(':id')
-    update(
-      @Param('id') id: number,
-      @Body() feedPost: FeedPost,
-    ): Observable<UpdateResult> {
-      return this.feedService.updatePost(id, feedPost);
-    }
-  
-    @Delete(':id')
-    delete(@Param('id') id: number): Observable<DeleteResult> {
-      return this.feedService.deletePost(id);
-    }
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Observable, skip } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @Post()
+  create(@Body() feedPost: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(feedPost);
   }
+
+  // @Get()
+  // findAll(): Observable<FeedPost[]> {
+  //   return this.feedService.findAllPosts();
+  // }
+
+  @Get()
+  findSelected(
+    @Query('take') take: number = 1,
+    @Query('skip') skip: number = 1,
+  ): Observable<FeedPost[]> {
+    take = take > 20 ? 20 : take;
+    return this.feedService.findPosts(take, skip);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
+  }
+}
